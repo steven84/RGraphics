c8dc13284411ac3824cdae17407bbf1d01df6b0f
steven84 <mayuqian0910@gmail.com>
2016-04-03 20:48:34 +0800

    complete file

--- explore.R
+++ explore.R
@@ -18,3 +18,31 @@ ggplot(pressure, aes(x=temperature, y=pressure)) + geom_line()
 
 qplot(temperature, pressure, data=pressure, geom=c("line", "point"))
 ggplot(pressure, aes(x=temperature, y=pressure)) + geom_line() + geom_point()
+
+?BOD
+barplot(BOD$demand, names.arg = BOD$Time)
+
+barplot(table(mtcars$cyl))
+
+qplot(mtcars$mpg)
+qplot(mpg, data = mtcars, binwidth=4)
+ggplot(mtcars, aes(x=mpg)) + geom_histogram(binwidth = 4)
+
+boxplot(len ~ supp, data = ToothGrowth)
+boxplot(len ~ supp + dose, data = ToothGrowth)
+
+qplot(supp, len, data = ToothGrowth, geom = "boxplot")
+ggplot(ToothGrowth, aes(supp,len)) + geom_boxplot()
+
+qplot(interaction(supp, dose), len, data = ToothGrowth,geom = "boxplot")
+ggplot(ToothGrowth, aes(interaction(supp, dose),len)) + geom_boxplot()
+
+curve(x^3 - 5*x, from=-4, to=4)
+
+myfun <- function(xvar) {
+    1/(1 + exp(-xvar + 10))
+}
+curve(myfun(x), from = 0, to = 20)
+curve(1-myfun(x), add = TRUE, col = "red")
+
+ggplot(data.frame(x=c(0, 20)), aes(x=x)) + stat_function(fun = myfun, geom = "line")
