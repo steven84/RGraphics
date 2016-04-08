687fd41482d7d60f09d288ac5ff1f686d8660478
steven84 <mayuqian0910@gmail.com>
2016-04-08 23:21:05 +0800

    modify on Lenovo

--- bar.R
+++ bar.R
@@ -1,4 +1,7 @@
 library(gcookbook)
+library(plyr)
+library(ggplot2)
+
 ?pg_mean
 str(pg_mean)
 ggplot(pg_mean, aes(x=group, y=weight)) + geom_bar(stat = "identity")
@@ -47,7 +50,6 @@ ggplot(cabbage_exp, aes(x=Date, y=Weight, fill=Cultivar)) +
     geom_bar(stat = "identity" ) +
     guides(fill=guide_legend(reverse = TRUE))
 
-library(plyr)
 ggplot(cabbage_exp, aes(x=Date, y=Weight, fill=Cultivar, order=desc(Cultivar))) +
     geom_bar(stat = "identity" )
 
@@ -56,8 +58,6 @@ ggplot(cabbage_exp, aes(x=Date, y=Weight, fill=Cultivar)) +
     guides(fill=guide_legend(reverse = TRUE)) +
     scale_fill_brewer(palette = "Pastel1")
 
-library(gcookbook)
-library(plyr)
 ce <- ddply(cabbage_exp, "Date", transform, percent_weight = Weight / sum(Weight) * 100)
 
 ggplot(ce, aes(x=Date, y=percent_weight, fill=Cultivar)) +
@@ -119,3 +119,31 @@ ggplot(tophit, aes(x=avg, y=reorder(name, avg))) +
           panel.grid.minor.x = element_blank(),
           panel.grid.major.y = element_line(colour = "gray60", linetype = "dashed"))
 
+ggplot(tophit, aes(y=avg, x=reorder(name, avg))) +
+    geom_point(size=3) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle=60,hjust=1),
+          panel.grid.major.y = element_blank(),
+          panel.grid.minor.y = element_blank(),
+          panel.grid.major.x = element_line(colour = "grey60",linetype = "dashed"))
+
+nameorder <- tophit$name[order(tophit$lg, tophit$avg)]
+
+tophit$name <- factor(tophit$name, levels=nameorder)
+
+ggplot(tophit, aes(x=avg, y=name)) +
+    geom_segment(aes(yend=name), xend=0, colour="grey50") +
+    geom_point(size=3, aes(colour=lg)) +
+    scale_colour_brewer(palette = "Set1", limits=c("NL","AL")) +
+    theme_bw() +
+    theme(panel.grid.major.y = element_blank(),
+          legend.position=c(1, 0.55),
+          legend.justification=c(1, 0.5))
+
+ggplot(tophit, aes(x=avg, y=name)) +
+    geom_segment(aes(yend=name), xend=0, colour="grey50") +
+    geom_point(size=3, aes(colour=lg)) +
+    scale_colour_brewer(palette = "Set1", limits=c("NL","AL"), guide=FALSE) +
+    theme_bw() +
+    theme(panel.grid.major.y = element_blank()) +
+    facet_grid(lg~., scales = "free_y", space = "free_y")
